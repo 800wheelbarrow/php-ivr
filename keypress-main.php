ad78569e3176cd64d1422c363611df82892b8df4
800wheelbarrow <31018771+800wheelbarrow@users.noreply.github.com>
2018-03-05 21:15:35 -0500

    clean up code and comments

--- keypress-main.php
+++ keypress-main.php
@@ -5,46 +5,31 @@ use Twilio\Twiml;
 $response = new Twiml();
 
 switch ($_REQUEST['Digits']) {
-    case 1:
-	$response->redirect('weather/menu.php', ['method' => 'POST']);
-	        // echo <<<EOD
-        // <Response>
-		// <Redirect method="POST">https://www.zaxz.pw/twilio/weather/menu.php</Redirect>
-        // </Response>
-// EOD;
+	case 1:
+		$response->redirect('weather/menu.php', ['method' => 'POST']);
 	break;
-   case 2:
-	$response->redirect('blackjack/menu.php', ['method' => 'POST']);
-	        // echo <<<EOD
-        // <Response>
-		// <Redirect method="POST">https://www.zaxz.pw/twilio/blackjack/menu.php</Redirect>
-        // </Response>
-// EOD;
-        break;
-		   case 3:
-	$response->redirect('news/menu.php', ['method' => 'POST']);
-        break;
-    case 4:
-	$response->redirect('gb/menu.php', ['method' => 'POST']);
-	        // echo <<<EOD
-        // <Response>
-		// <Redirect method="POST">https://www.zaxz.pw/twilio/gb/menu.php</Redirect>
-	    // </Response>
-// EOD;
-		break;
-    case 5:
-	$response->redirect('po/menu.php', ['method' => 'POST']);
-           // echo <<<EOD
-        // <Response>
-        // <Redirect method="POST">https://www.zaxz.pw/twilio/po/menu.php</Redirect>
-        // </Response>
-// EOD;
-        break;
-	//If any other key is pressed, return to the main menu script
-	default:
-	 header("Location: menu-main.php");
-        die;
-}
-echo $response;
+	
+	case 2:
+		$response->redirect('blackjack/menu.php', ['method' => 'POST']);
+	break;
+	
+	case 3:
+		$response->redirect('news/menu.php', ['method' => 'POST']);
+	break;
 
+	case 4:
+		$response->redirect('gb/menu.php', ['method' => 'POST']);
+	break;
+	
+	case 5:
+		$response->redirect('po/menu.php', ['method' => 'POST']);
+	break;
+	
+	//If any other key is pressed, return to the main menu
+	default:
+		header("Location: menu-main.php");
+	die;
+}
+
+echo $response;
 ?>
--- menu-main.php
+++ menu-main.php
@@ -3,11 +3,11 @@ require_once './vendor/autoload.php';
 use Twilio\Twiml;
 
 $response = new Twiml();
-$intro = $response->say('Welcome to the cafe eighties');
-$pause = $response->pause(['length' => 1]);
-$gather = $response->gather(['action' => 'keypress-main.php','input' => 'dtmf', 'timeout' => 5, 'numDigits' => 1]);
-$gather->say('For weather, press 1. For blackjack, press 2. For news, press 3. For George Benson, press 4. For The Police, press 5.');
-$response->redirect('menu-main.php', ['method' => 'POST']);
+	$intro = $response->say('Welcome to the cafe eighties');
+	$pause = $response->pause(['length' => 1]);
+	$gather = $response->gather(['action' => 'keypress-main.php','input' => 'dtmf', 'timeout' => 5, 'numDigits' => 1]);
+		$gather->say('For weather, press 1. For blackjack, press 2. For news, press 3. For George Benson, press 4. For The Police, press 5.');
+	$response->redirect('menu-main.php', ['method' => 'POST']);
 
 echo $response;
 ?>
