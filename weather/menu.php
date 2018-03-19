1340ad8574312c7a1356b140498299c29276bb76
800wheelbarrow <31018771+800wheelbarrow@users.noreply.github.com>
2018-03-18 20:45:33 -0400

    Remove unnecessary "press pound" speech

--- weather/menu.php
+++ weather/menu.php
@@ -5,7 +5,7 @@ use Twilio\Twiml;
 $response = new Twiml();
 $intro = $response->say('Welcome to the weather service.');
 $gather = $response->gather(['action' => 'keypress.php','input' => 'dtmf', 'timeout' => 10, 'numDigits' => 5]);
-$gather->say('Enter a zip code and press pound. ');
+$gather->say('Enter a five digit zip code now. ');
 // $response->redirect('menu-main.php', ['method' => 'POST']);
 
 echo $response;
